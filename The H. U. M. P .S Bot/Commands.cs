424568894e9902da10d5d79edb13de60bd081d41
Jonathan <lokkhj@gmail.com>
2019-11-20 21:07:10 -0500

    Added preconditions

--- The H. U. M. P .S Bot/Commands.cs
+++ The H. U. M. P .S Bot/Commands.cs
@@ -10,14 +10,17 @@ namespace THUMPSBot
         SaveTest save = new SaveTest();
 
         [Command("test")]
+        [RequireOwner(ErrorMessage = "This is a test command and is not intended for public use.", Group = "Permmision")]
         [Summary("A test command")]
         public async Task Test() => await ReplyAsync("test");
 
         [Command("inputTest")]
+        [RequireOwner(ErrorMessage = "This is a test command and is not intended for public use.", Group = "Permmision")]
         [Summary("Tests input by sending a echo")]
         public async Task InputTest([Remainder]string input) => await ReplyAsync(input);
 
         [Command("save")]
+        [RequireOwner(ErrorMessage = "This is a test command and is not intended for public use.", Group = "Permmision")]
         [Summary("Test saving mechanics")]
         public async Task SaveTest([Remainder] string input)
         {
@@ -25,6 +28,7 @@ namespace THUMPSBot
             await ReplyAsync("Saved: \"" + input + "\" to text file. Do !load to view all saved items");
         }
         [Command("load")]
+        [RequireOwner(ErrorMessage = "This is a test command and is not intended for public use.", Group = "Permmision")]
         [Summary("Test loading mechanics")]
         public async Task LoadTest()
         {
@@ -36,8 +40,9 @@ namespace THUMPSBot
     public class InDevModule : ModuleBase<SocketCommandContext>
     {
         Mod_Actions actions = new Mod_Actions();
+
         [Command("infractions")]
-        
+        [RequireOwner(ErrorMessage = "This command is in development. You cannot use it right now.", Group = "Permmision")]
         [Summary("Finds infractions")]
         public async Task Infractions([Remainder] Discord.IUser user)
         {
